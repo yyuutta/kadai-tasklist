69b957ebd56f4462879001a2d78ec99ef7103fdf
yyuutta <ayaragi.yamayu@gmail.com>
2018-07-12 13:25:14 +0000

    Bootstrap-kadai-again

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,10 +5,7 @@
     <h1>タスク新規作成ページ</h1>
 
     <div class="row">
-        <div class="col-xs-12">
-        <div class="col-sm-offset-2 col-sm-8">
-        <div class="col-md-offset-2 col-md-8">
-        <div class="col-lg-offset-3 col-lg-6">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
             {!! Form::model($task, ['route' => 'tasks.store']) !!}
                 
                 <div class="form-group">
@@ -24,9 +21,6 @@
                 {!! Form::submit('投稿') !!}
         
             {!! Form::close() !!}
-        </div>
-        </div>
-        </div>
         </div>
     </div>
     
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -5,12 +5,9 @@
     <h1>id: {{ $task->id }} のタスク編集ページ</h1>
 
     <div class="row">
-        <div class="col-xs-12">
-        <div class="col-sm-offset-2 col-sm-8">
-        <div class="col-md-offset-2 col-md-8">
-        <div class="col-lg-offset-3 col-lg-6">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
             {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
-                
+
                 <div class="form-group">
                     {!! Form::label('status', 'ステータス:') !!}
                     {!! Form::text('status', null, ['class' => 'form-control']) !!}
@@ -24,9 +21,6 @@
                 {!! Form::submit('更新') !!}
         
             {!! Form::close() !!}
-        </div>
-        </div>
-        </div>
         </div>
     </div>
     
